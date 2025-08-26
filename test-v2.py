034d8a867cd9412bc7998124c4121ec3734cffae
Gilles GUYONNET <gil.guyonnet@gmail.com>
2025-08-26 18:04:16 +0200

    Commit Branch-1

---
+++ test-v2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+
+if __name__ == '__main__':
+    os.system('clear')
+
+    result: float = 3.14 * 12
+    print(f'The result is {result:.2f}')
+
+    letters = ["a"]
+    class Character:
+        letters = ["b"]
+        letters = letters + ["c"]
+
+    print(letters[0], Character.letters)
+    print("\nEnd")
